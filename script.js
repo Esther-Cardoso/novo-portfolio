be648116557bae2a722fca2fd06f227fbda9e50d
Esther Cardoso <esthercardosofernandes@gmail.com>
2023-05-26 10:27:02 -0300

    filter card section projetos

--- script.js
+++ script.js
@@ -17,3 +17,28 @@ function mudarTema() {
 
 mudarTema()
 
+function filterElement() {
+  const filterItem = document.querySelector(".items");
+  const filterCard = document.querySelectorAll(".card");
+  
+  window.onload = () => {
+    filterItem.onclick = (selectedItem) => {
+      if (selectedItem.target.classList.contains("item")) {
+        filterItem.querySelector(".active").classList.remove("active");
+        selectedItem.target.classList.add("active");
+        let filterName = selectedItem.target.getAttribute("data-name");
+        filterCard.forEach((card) => {
+          let filterCard = card.getAttribute("data-name");
+          if (filterCard == filterName || filterName == "todos") {
+            card.classList.add("show");
+          } else {
+            card.classList.add("hide");
+            card.classList.remove("show");
+          }
+        });
+      }
+    };
+  };  
+}
+
+filterElement()
